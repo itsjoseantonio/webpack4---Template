33ef77c497b254a1e1affcbab41df8be34e0e813
TH <e.vilela.m@gmail.com>
2018-04-07 23:38:22 -0500

    adding query-string for access_token url

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../style/index.sass';
+/*import '../style/index.sass';
 import fakeData from '../json/fakedata.json'
 
 let testing = (()=>console.log('its working'))
@@ -9,4 +9,55 @@ for(let i in arreglo){
 }
 testing(); 
 
-console.log(fakeData)
+console.log(fakeData)*/
+
+import queryString from 'query-string'
+
+let parsed = queryString.parse(window.location.search)
+let accessToken = parsed.access_token;
+
+var artist = document.getElementById('artist');
+var button = document.getElementById('button')
+button.addEventListener('click', getArtist)
+
+function getArtist(){
+    fetch(`https://api.spotify.com/v1/search?type=artist&limit=50&q=${artist.value}`,{
+        headers:{
+            'Authorization': 'Bearer ' + accessToken
+        }
+    }).then( response => response.json() )
+    .then(data => {
+        var result = data;
+        var firstId = result.artists.items[0].id;
+        return firstId;
+    }).then(id => {
+        let idArtist = id;
+        console.log('artista')
+        console.log(idArtist)
+        fetch(`https://api.spotify.com/v1/artists/${idArtist}/albums`,{
+            headers:{
+                'Authorization': 'Bearer ' + accessToken
+            }
+        })
+        .then(response => response.json())
+        .then(albumData => {
+            let albumID = albumData.items[0].id;
+            console.log('album')
+            console.log(albumID)
+            fetch(`https://api.spotify.com/v1/albums/${albumID}/tracks`,{
+                headers:{
+                    'Authorization': 'Bearer ' + accessToken
+                }
+            }).then(response => response.json())
+            .then(canciones => {
+                console.log(canciones)
+                let cancionList = []
+                let Acanciones = canciones.items;
+                for (var cancion in Acanciones){
+                    cancionList.push(Acanciones[cancion].name)
+                }
+                console.log(cancionList)
+            })
+        })
+    })
+}
