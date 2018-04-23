6712b8e7f1d07e0f8506af19687a4a50cf038aaf
TH <e.vilela.m@gmail.com>
2018-04-22 19:53:57 -0500

    add image suport (jpg|png|svg)

--- src/js/ubigeo.js
+++ src/js/ubigeo.js
@@ -1,41 +1,48 @@
 module.exports = {
-    fillUbigeo: function Ubigeo(dataUbigeo,idDepartamento,idProvincia,idDistrito,texto = 'Seleccione'){
-        const ubigeoData = new Promise((resolve, reject) => {
-            resolve(dataUbigeo)
-        }).then(data => {
-            const selectDepartamento = document.getElementById(idDepartamento);
-            const selectProvincia = document.getElementById(idProvincia)
-            const selectDistrito = document.getElementById(idDistrito)
-            let selectDepartamentoValue = "";
-            selectDepartamento.insertAdjacentHTML('afterbegin', `<option>${texto}</option>`)
-            selectProvincia.insertAdjacentHTML('afterbegin', `<option>${texto}</option>`)
-            selectDistrito.insertAdjacentHTML('afterbegin', `<option>${texto}</option>`)
-            for (const index in data) {
-                selectDepartamento.insertAdjacentHTML('beforeend', `<option value="${data[index].idDepart}">${data[index].nomDepart}</option>`)
-            }
-        
-            selectDepartamento.addEventListener('change', (el) => {
-                selectDepartamentoValue = parseInt(el.currentTarget.value)-1
-                selectProvincia.querySelectorAll('option').forEach(e => e.parentNode.removeChild(e));
-                selectDistrito.querySelectorAll('option').forEach(e => e.parentNode.removeChild(e));
-                selectProvincia.insertAdjacentHTML('afterbegin', `<option>${texto}</option>`)
-                selectDistrito.insertAdjacentHTML('afterbegin', `<option>${texto}</option>`)
-                const provincias = data[parseInt(el.currentTarget.value)-1].provincias
-                for(const index in provincias){
-                    selectProvincia.insertAdjacentHTML('beforeend', `<option value="${provincias[index].idProvincia}">${provincias[index].nomProvincia}</option>`)
-                }
-            })
-        
-            selectProvincia.addEventListener('change', (el) => {
-                selectDistrito.querySelectorAll('option').forEach(el => el.parentNode.removeChild(el));
-                selectDistrito.insertAdjacentHTML('afterbegin', `<option>${texto}</option>`)
-                const distritos = data[selectDepartamentoValue].provincias[parseInt(el.currentTarget.value)-1].distritos;
-                for(const index in distritos){
-                    selectDistrito.insertAdjacentHTML('beforeend', `<option value="${distritos[index].idDistrito}">${distritos[index].nomDistrito}</option>`)
-                }
-            })
-        })
-    }
-}
+  fillUbigeo: function Ubigeo(dataUbigeo, idDepartamento, idProvincia, idDistrito, texto = "Seleccione") {
+    const ubigeoData = new Promise((resolve, reject) => {
+      resolve(dataUbigeo);
+    }).then(data => {
+      const selectDepartamento = document.getElementById(idDepartamento);
+      const selectProvincia = document.getElementById(idProvincia);
+      const selectDistrito = document.getElementById(idDistrito);
+      let selectDepartamentoValue = "";
+      selectDepartamento.insertAdjacentHTML("afterbegin", `<option>${texto}</option>`);
+      selectProvincia.insertAdjacentHTML("afterbegin", `<option>${texto}</option>`);
+      selectDistrito.insertAdjacentHTML("afterbegin", `<option>${texto}</option>`);
+      for (const index in data) {
+        selectDepartamento.insertAdjacentHTML(
+          "beforeend",
+          `<option value="${data[index].idDepart}">${data[index].nomDepart}</option>`
+        );
+      }
 
+      selectDepartamento.addEventListener("change", el => {
+        selectDepartamentoValue = parseInt(el.currentTarget.value) - 1;
+        selectProvincia.querySelectorAll("option").forEach(e => e.parentNode.removeChild(e));
+        selectDistrito.querySelectorAll("option").forEach(e => e.parentNode.removeChild(e));
+        selectProvincia.insertAdjacentHTML("afterbegin", `<option>${texto}</option>`);
+        selectDistrito.insertAdjacentHTML("afterbegin", `<option>${texto}</option>`);
+        const provincias = data[parseInt(el.currentTarget.value) - 1].provincias;
+        for (const index in provincias) {
+          selectProvincia.insertAdjacentHTML(
+            "beforeend",
+            `<option value="${provincias[index].idProvincia}">${provincias[index].nomProvincia}</option>`
+          );
+        }
+      });
 
+      selectProvincia.addEventListener("change", el => {
+        selectDistrito.querySelectorAll("option").forEach(el => el.parentNode.removeChild(el));
+        selectDistrito.insertAdjacentHTML("afterbegin", `<option>${texto}</option>`);
+        const distritos = data[selectDepartamentoValue].provincias[parseInt(el.currentTarget.value) - 1].distritos;
+        for (const index in distritos) {
+          selectDistrito.insertAdjacentHTML(
+            "beforeend",
+            `<option value="${distritos[index].idDistrito}">${distritos[index].nomDistrito}</option>`
+          );
+        }
+      });
+    });
+  }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,74 @@
-const path = require('path');
+const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: {
-        index: path.resolve(__dirname, 'src/js/index.js')
-    },
-    output:{
-        path: path.resolve(__dirname,'dist'),
-        filename: 'js/[name].js'
-    },
-    module:{
-        rules: [
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    use: 'css-loader'
-                })
-            },
-            {
-                test: /\.(scss|sass)$/,
-                use: ExtractTextPlugin.extract({
-                    use: ['css-loader','sass-loader']
-                })
-            },
-            {
-                test: /\.pug$/,
-                use: ['html-loader','pug-html-loader']
-            },
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                  loader: 'babel-loader',
-                  options: {
-                    presets: ['env']
-                  }
-                }
-            }
-        ]
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'dist'),
-        port: 3000,
-        //abre el servidor, ejecutando el html en el browser que tengamos por defecto
-        open: true,
-        // indicar a webpack que html usar, como index, funciona siempre y cuando no exista un html con nombre index
-        // ya que esta funcion se ejecuta si es que le pasa algo al archivo principal, en este caso index.html
-        // al no tener este archivo no sabe a donde apuntar y se puede elegir cual cargar
-        historyApiFallback: {
-            index: 'another.html'
+  entry: {
+    index: path.resolve(__dirname, "src/js/index.js")
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: ExtractTextPlugin.extract({
+          use: ["css-loader", "sass-loader"]
+        })
+      },
+      {
+        test: /\.pug$/,
+        use: ["html-loader", "pug-html-loader"]
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          useRelativePath: true
         }
       },
-    plugins:[
-        new ExtractTextPlugin('css/[name].css'),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/pug/index.pug',
-        }),
-        /*new HtmlWebpackPlugin({
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env"]
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    port: 3000,
+    //abre el servidor, ejecutando el html en el browser que tengamos por defecto
+    open: true,
+    // indicar a webpack que html usar, como index, funciona siempre y cuando no exista un html con nombre index
+    // ya que esta funcion se ejecuta si es que le pasa algo al archivo principal, en este caso index.html
+    // al no tener este archivo no sabe a donde apuntar y se puede elegir cual cargar
+    historyApiFallback: {
+      index: "another.html"
+    }
+  },
+  plugins: [
+    new ExtractTextPlugin("css/[name].css"),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/pug/index.pug"
+    })
+    /*new HtmlWebpackPlugin({
             filename: 'another.html',
             template: './src/pug/another.pug',
         })*/
-    ]
-}
+  ]
+};
