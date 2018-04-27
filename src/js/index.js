f471a0873e5354c5f87b976be43d93f5673fa3e4
TH <e.vilela.m@gmail.com>
2018-04-27 00:36:36 -0500

    cambiando prettier config

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,20 @@
-import "../style/index.scss";
+import '../style/index.scss';
 
-import $ from "jquery";
-import { TimelineMax, TweenMax } from "gsap";
-import test from "./vendor/test";
-import ubigeo from "../json/ubigeo.json";
-import { fillUbigeo } from "./ubigeo.js";
+import $ from 'jquery';
+import { TimelineMax, TweenMax } from 'gsap';
+import test from './vendor/test';
+import ubigeo from '../json/ubigeo.json';
+import { fillUbigeo } from './ubigeo.js';
 
-fillUbigeo(ubigeo, "departamento", "provincia", "distrito");
+fillUbigeo(ubigeo, 'departamento', 'provincia', 'distrito');
 
-$("#button").on("click", function() {
-    tl
-        .to($("#prueba"), 0.5, { opacity: 0.5 })
-        .to($("#prueba"), 0.5, { scale: 0.5 })
-        .to($("#prueba"), 0.5, { rotation: 360 })
-        .to($("#prueba"), 0.5, { opacity: 1 })
-        .to($("#prueba"), 0.5, { scale: 1.5 });
+$('#button').on('click', function() {
+  tl
+    .to($('#prueba'), 0.5, { opacity: 0.5 })
+    .to($('#prueba'), 0.5, { scale: 0.5 })
+    .to($('#prueba'), 0.5, { rotation: 360 })
+    .to($('#prueba'), 0.5, { opacity: 1 })
+    .to($('#prueba'), 0.5, { scale: 1.5 });
 });
 
 const tl = new TimelineMax();
